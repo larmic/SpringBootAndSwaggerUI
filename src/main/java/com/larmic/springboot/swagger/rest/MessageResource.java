0022b6e47427f57860bd778f5d86126e17217b28
Lars Michaelis <mail@larmic.de>
2016-08-05 09:16:51 +0200

    fixed problem

--- src/main/java/com/larmic/springboot/swagger/rest/MessageResource.java
+++ src/main/java/com/larmic/springboot/swagger/rest/MessageResource.java
@@ -8,12 +8,13 @@ import org.springframework.web.bind.annotation.*;
 public class MessageResource {
 
     @ApiOperation(value = "simple message resource")
-    @ApiImplicitParams({
-            @ApiImplicitParam(name = "message", value = "Message to send", required = true, dataType = "com.larmic.springboot.swagger.rest.dto.MessageDto", paramType = "body")
-    })
+    // Its a bug: https://github.com/springfox/springfox/issues/1344
+    //@ApiImplicitParams({
+    //        @ApiImplicitParam(name = "message", value = "Message to send", required = true, dataType = "com.larmic.springboot.swagger.rest.dto.MessageDto", paramType = "body")
+    //})
     @RequestMapping(value = "/api/message", method = RequestMethod.POST,
             consumes = {"application/json", "application/xml"})
-    public void sendMessage(@RequestBody MessageDto message) {
+    public void sendMessage(@ApiParam(name = "message", value = "Message to send", required = true) @RequestBody MessageDto message) {
         System.out.println("ping");
     }
 }
